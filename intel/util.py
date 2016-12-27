b2c102b94e032f4cdc35e6b6a52716b347dc8d8f
Connor Doyle <connor.p.d@gmail.com>
2016-12-27 12:24:40 -0800

    Renamed discover=>init, added hugepages check.

--- intel/util.py
+++ intel/util.py
@@ -1,5 +1,18 @@
+import logging
 from os.path import normpath, realpath, join, pardir
 
 
 def kcm_root():
     return normpath(realpath(join(__file__, pardir, pardir)))
+
+
+def check_hugepages():
+    fd = open("/proc/meminfo", "r")
+    content = fd.read()
+    lines = content.split("\n")
+    for line in lines:
+        if line.startswith("HugePages_Free"):
+            parts = line.split()
+            num_free = int(parts[1])
+            if num_free == 0:
+                logging.warning("Warning: no hugepages are free")
