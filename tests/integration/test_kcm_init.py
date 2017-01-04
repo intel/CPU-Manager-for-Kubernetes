c037841e0021a0bdfadfb1ee0707b692b24f5a1a
Niklas Q. Nielsen <nik@qni.dk>
2017-01-04 14:53:14 -0800

    Reintroduced negative test

--- tests/integration/test_kcm_init.py
+++ tests/integration/test_kcm_init.py
@@ -3,10 +3,24 @@ from intel import init
 import os
 import pytest
 import tempfile
+import subprocess
+
+
+@pytest.mark.skipif(len(init.discover_topo()) >= 6,
+                    reason="""skipping negative test if enough physical
+                              cores are available""")
+def test_kcm_init_insufficient_cores():
+    args = ["init",
+            "--conf-dir={}".format(os.path.join(tempfile.mkdtemp(), "init"))]
+
+    # Expect to fail if system has insufficient number of cores.
+    with pytest.raises(subprocess.CalledProcessError):
+        integration.execute(integration.kcm(), args)
 
 
 @pytest.mark.skipif(len(init.discover_topo()) < 6,
-                    reason="requires at least 6 physical cores to pass")
+                    reason="""skipping test if system has insufficient
+                              number of physical cores""")
 def test_kcm_init():
     args = ["init",
             "--conf-dir={}".format(os.path.join(tempfile.mkdtemp(), "init"))]
