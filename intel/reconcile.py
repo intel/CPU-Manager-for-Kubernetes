05a2d478635473c46c4ffb0a8e789e0c5fd62ff4
Connor Doyle <connor.p.d@gmail.com>
2017-01-04 22:07:14 -0800

    Read process info from configurable procfs.

--- intel/reconcile.py
+++ intel/reconcile.py
@@ -1,5 +1,4 @@
-from . import config
-import psutil
+from . import config, proc
 
 
 def reconcile(conf_dir):
@@ -9,5 +8,5 @@ def reconcile(conf_dir):
         for pool_name, pool in c.pools().items():
             for cl in pool.cpu_lists().values():
                 for task in cl.tasks():
-                    if not psutil.pid_exists(task):
+                    if not proc.Process(task).exists():
                         cl.remove_task(task)
