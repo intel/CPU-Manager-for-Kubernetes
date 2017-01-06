a148d6240cacedac7dd7fec289f14a486fdceea1
Niklas Q. Nielsen <nik@qni.dk>
2017-01-06 10:00:17 -0800

    Removed last diff statements

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 import subprocess
+import copy
 
 
 # Returns the absolute path to the test config directory with the supplied
@@ -17,7 +18,12 @@ def rand_str(length=8, chars=string.ascii_lowercase + string.digits):
 
 # Returns resulting stdout buffer from interpreting the supplied command with
 # a shell. Raises process errors if the command exits nonzero.
-def execute(cmd, args=[]):
+def execute(cmd, args=[], env=None):
     cmd_str = "{} {}".format(cmd, " ".join(args))
-    stdout = subprocess.check_output(cmd_str, shell=True)
+
+    host_env = copy.deepcopy(os.environ)
+    if env is not None:
+        host_env.update(env)
+
+    stdout = subprocess.check_output(cmd_str, shell=True, env=host_env)
     return stdout
--- tests/integration/integration.py
+++ tests/integration/integration.py
@@ -1,10 +1,5 @@
 from intel import util
 import os
-<<<<<<< HEAD
-import subprocess
-import copy
-=======
->>>>>>> master
 
 
 # Returns the absolute path to the top-level kcm script.
