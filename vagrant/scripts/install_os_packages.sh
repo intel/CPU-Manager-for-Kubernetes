cb5e1622b847bb45e63564317d36c5b3c64d7e03
Szymon Scharmach <szymon.scharmach@intel.com>
2017-01-09 15:03:27 +0100

    Disable guest additions & run init script inside toolbox

--- vagrant/scripts/install_os_packages.sh
+++ vagrant/scripts/install_os_packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Refreshing state..."
-apt-get update
+
+TOOLBOX_CMD="toolbox -q"
+
 echo "Updating system..."
-apt-get upgrade -y -q
-apt-get dist-upgrade -y -q
+$TOOLBOX_CMD dnf update -y
 echo "Installing software dependencies..."
-apt-get install -y \
-    python-dev \
+$TOOLBOX_CMD dnf install -y \
+    python-devel
     python-pip
