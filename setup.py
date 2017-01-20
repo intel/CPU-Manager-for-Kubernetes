10260bb35f42ec21ef888a8a8615fb68aa7481d8
Connor Doyle <connor.p.d@gmail.com>
2017-01-20 12:04:25 -0800

    Updated version in setup.py.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@
 from setuptools import setup
 
 setup(name='kcm',
-      version='0.1.0',
+      version='0.1.1',
       description='Support minimal comms use cases on Kubernetes',
       url='http://github.com/intelsdi-x/kubernetes-comms-mvp',
       author='Intel SDI-E',
