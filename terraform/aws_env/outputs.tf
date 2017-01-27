69ef7af8a08c90a9230c9e36a8756ea972d99600
squall0gd <michal.stachowski@outlook.com>
2017-01-27 14:16:29 +0100

    Updated output

--- terraform/aws_env/outputs.tf
+++ terraform/aws_env/outputs.tf
@@ -1,24 +1,24 @@
 output "connectivity_commands" {
-    value = ["${
+  value = ["${
 		formatlist("ssh %s@%s", var.user_name[0], concat(aws_instance.aws_minions.*.public_ip, aws_instance.aws_master.*.public_ip, aws_instance.aws_etcd.*.public_ip))}"]
 }
 
-output "ip_addresses" {
-    value = "${module.aggregator.ip_map}"
+output "mapped_ips" {
+  value = "${module.aggregator.ip_map}"
 }
 
-output "users" {
-    value = "${module.aggregator.user_map}"
+output "mapped_users" {
+  value = "${module.aggregator.user_map}"
 }
 
-output "ports" {
-    value = "${module.aggregator.port_map}"
+output "mapped_ports" {
+  value = "${module.aggregator.port_map}"
 }
 
 output "private_keys" {
-    value = "${module.aggregator.pk_map}"
+  value = "${module.aggregator.pk_map}"
 }
 
 output "used_os" {
-    value = "${var.os_type}"
+  value = "${var.os_type}"
 }
