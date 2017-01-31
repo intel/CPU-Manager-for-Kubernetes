ea737349df32785b9f69198bd4037da8065c0033
Niklas Q. Nielsen <nik@qni.dk>
2017-01-31 14:00:13 -0800

    Snapshot of isol cmdline topology

--- tests/unit/test_topology.py
+++ tests/unit/test_topology.py
@@ -140,3 +140,39 @@ def test_isolcpus_valid_input():
                "root=/dev/md2 ro net.ifnames=0 isolcpus=0,1,2,3,8,9,10,11")
 
     assert topology.isolcpus(cmdline) == [0, 1, 2, 3, 8, 9, 10, 11]
+
+
+def test_topology_isolated():
+    lscpu = """#The following is the parsable format, which can be fed to other
+# programs. Each different item in every column has an unique ID
+# starting from zero.
+# CPU,Core,Socket,Node,,L1d,L1i,L2,L3
+0,0,0,0,,0,0,0,0
+1,1,0,0,,1,1,1,0
+2,2,0,0,,2,2,2,0
+3,3,0,0,,3,3,3,0
+4,0,0,0,,0,0,0,0
+5,1,0,0,,1,1,1,0
+6,2,0,0,,2,2,2,0
+7,3,0,0,,3,3,3,0
+"""
+
+    isolated_cpus = [0, 4, 1, 5]
+
+    sockets = topology.parse(lscpu, isolated_cpus)
+    assert len(sockets) == 1
+
+    cpumap = sockets[0].cores
+    assert len(cpumap) == 4
+    assert 0 in cpumap
+    assert 1 in cpumap
+    assert 2 in cpumap
+    assert 3 in cpumap
+    assert cpumap[0].cpu_ids() == [0, 4]
+    assert cpumap[0].is_isolated()
+    assert cpumap[1].cpu_ids() == [1, 5]
+    assert cpumap[1].is_isolated()
+    assert cpumap[2].cpu_ids() == [2, 6]
+    assert not cpumap[2].is_isolated()
+    assert cpumap[3].cpu_ids() == [3, 7]
+    assert not cpumap[3].is_isolated()
